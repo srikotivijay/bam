b0c449307751a9892cd1f22cf2cff076a9a34082
Gauthier Devolder <gdevolder@dow.com>
2017-08-07 02:15:04 +0000

    enable plants based on user role and plant status

--- webapp/controller/GMIDPlantAssignment.controller.js
+++ webapp/controller/GMIDPlantAssignment.controller.js
@@ -15,15 +15,10 @@ sap.ui.define([
 		//
 		onInit : function () {
 			
-			// define a global variable for the oData model		    
-		    this._oDataModel = new sap.ui.model.odata.ODataModel("/ODataService/BAMDataService.xsodata/", true);
 		    // Create view model for the page
 		    var oModel = new sap.ui.model.json.JSONModel();
 
-	    	//var oi18nModel = new ResourceModel({
-              //  bundleName: "bam.i18n.i18n"
-           // });
-            		var groupedGMIDPlantCountry = [];
+            var groupedGMIDPlantCountry = [];
                 
 		        //common code to check duplicates
 				var hash = (function() {
@@ -52,15 +47,48 @@ sap.ui.define([
 			var gmidPlantAssignmentRecords = [];
 		    // Get the GMID Plant combinations for the GMID-Country combination selected by the user
 		    var viewpath = "V_GMID_COUNTRY_PLANT_STATUS";
-		    	if(this._gmidIDsList !== undefined)
-					{
-			             // get all the GMID/Plants data for the GMIDS entered in UI
-			             gmidPlantAssignmentRecords = DataContext.getGMIDCountryPlantListFromDB(this._gmidIDsList,viewpath); 
-					}
+	    	if(this._gmidIDsList !== undefined)
+			{
+	             // get all the GMID/Plants data for the GMIDS entered in UI
+	             gmidPlantAssignmentRecords = DataContext.getGMIDCountryPlantListFromDB(this._gmidIDsList,viewpath); 
+			}
+			
+			//resource model
+			var oi18nModel = new ResourceModel({
+                	bundleName: "bam.i18n.i18n"
+            	});
+            	
+        	// get the GMID plant status for i18n model
+	    	var z1gmid = oi18nModel.getProperty("z1gmidstatus");
+	    	var zcgmid = oi18nModel.getProperty("zcgmidstatus");
+	    	var z9gmid = oi18nModel.getProperty("z9gmidstatus");
+            var activePlantStatus = oi18nModel.getProperty("activePlantStatus");
+             
+			// get the Module settings for i18n model
+    		var plantAssignment = oi18nModel.getProperty("Module.plantAssignment");
+    		var actionAdd = oi18nModel.getProperty("Module.actionAdd");
+    		
+    		// defining add permision for plant assignment, default to false
+    		var permissionToAdd = false;
+			
+			// getting permissions for the current logged in user
+			var permissions = DataContext.getUserPermissions();
+			// check to see if the permission list includes "Add" action for the PLANT ASSIGNMENT Module
+			// ATTRIBUTE in this case means MODULE
+			for(var i = 0; i < permissions.length; i++)
+			{
+				if(permissions[i].ATTRIBUTE === plantAssignment && permissions[i].ACTION === actionAdd)
+				{
+					permissionToAdd = true;
+					// break since the user may have more than one role, as long as one of the user roles has permission to edit we can show the button
+					break;
+				}
+			}
 					
 			if (gmidPlantAssignmentRecords.length !==0){
 					//loop through the rows of the retruened data
-					for (var i = 0; i < gmidPlantAssignmentRecords.length; i++) {
+					for (var i = 0; i < gmidPlantAssignmentRecords.length; i++) 
+					{
 						var item =  gmidPlantAssignmentRecords[i];
 						key = item.GMID + ";" + item.COUNTRY;
 					    //check for the gmid and country combination key 
@@ -73,7 +101,25 @@ sap.ui.define([
 					        						 COUNTRY:item.COUNTRY,
 					        						 PLANTS:[]});
 						}
+						
+						// setting permission to edit plant based on user role & plant status
+						if(permissionToAdd && (item.PLANT_STATUS !== z1gmid) && (item.PLANT_STATUS !== zcgmid) && (item.PLANT_STATUS !== z9gmid))
+						{
+							item.IS_EDITABLE = true;
+						}
+						else 
+						{
+							item.IS_EDITABLE = false;
+						}
+						
+						// setting plant status to Active if the plant status is null or blank
+						if(item.PLANT_STATUS === null)
+						{
+							item.PLANT_STATUS = activePlantStatus;
+						}
+						
 						// check if the plant code is already associated with GMID Country combination
+						// used to turn string value into boolean
 						// if associated make it as selected
 						if (item.IS_SELECTED === "false")
 						{
@@ -82,9 +128,11 @@ sap.ui.define([
 						else
 						{
 							item.IS_SELECTED = true;
+							item.IS_EDITABLE = false;
 						}
+						
 					    //find the object for the gmid and country combination and push the plant code to the nested plant object
-					    groupedGMIDPlantCountry.find(function(data){return data.GMID === item.GMID && data.COUNTRY === item.COUNTRY;}).PLANTS.push({PLANT_CODE: item.PLANT_CODE,IS_SELECTED : item.IS_SELECTED,PLANT_STATUS: item.PLANT_STATUS, PLANT_STATUS_DESC: item.PLANT_STATUS_DESC});
+					    groupedGMIDPlantCountry.find(function(data){return data.GMID === item.GMID && data.COUNTRY === item.COUNTRY;}).PLANTS.push({PLANT_CODE: item.PLANT_CODE,IS_SELECTED : item.IS_SELECTED,PLANT_STATUS: item.PLANT_STATUS, IS_EDITABLE : item.IS_EDITABLE, PLANT_STATUS_DESC: item.PLANT_STATUS_DESC});
 					}
 			}
 				
