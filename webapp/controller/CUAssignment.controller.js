742d881e8fcd59dab8f7bc63da128feff236bfff
Ganesh Kulkarni <gkulkarni@dow.com>
2018-03-13 20:59:47 +0000

    S2 US 45

---
+++ webapp/controller/CUAssignment.controller.js
@@ -0,0 +1,62 @@
+sap.ui.define([
+		"sap/ui/core/mvc/Controller",
+		"bam/services/DataContext",
+		"sap/ui/model/resource/ResourceModel"
+	], function (Controller,DataContext,ResourceModel) {
+		"use strict";
+
+  	var loggedInUserID;
+	var firstTimePageLoad = true;
+	return Controller.extend("bam.controller.CUAssignment", {
+			onInit : function () {
+				// Get logged in user id
+			    loggedInUserID = DataContext.getUserID();
+				 // define a global variable for the oData model		    
+		    	var oView = this.getView();
+		    	oView.setModel(this.getOwnerComponent().getModel());
+	    		
+	    		//remove the selection column
+	    		var oSmartTable = this.getView().byId("smartTblCUAssignment");     //Get Hold of smart table
+				var oTable = oSmartTable.getTable();          //Analytical Table embedded into SmartTable
+				oTable.setSelectionMode("None");
+				oTable.setEnableColumnFreeze(true);
+				//oTable.getColumns();
+		    	
+		    	if(firstTimePageLoad)
+		    	{
+		    		//attach _onRouteMatched to be called everytime on navigation to Maintain Attributes page
+		    		var oRouter = this.getRouter();
+					oRouter.getRoute("cuAssignment").attachMatched(this._onRouteMatched, this);
+		    	}
+			},
+			getRouter : function () {
+				return sap.ui.core.UIComponent.getRouterFor(this);
+			},
+			// force init method to be called everytime we naviagte to Maintain Attribuets page 
+			_onRouteMatched : function (oEvent) {
+				// If the user does not exist in the BAM database, redirect them to the denied access page
+				if(DataContext.isBAMUser() === false)
+				{
+					this.getOwnerComponent().getRouter().navTo("accessDenied");
+				}
+				else
+				{
+					if(firstTimePageLoad)
+					{
+						firstTimePageLoad = false;
+					}
+					else
+					{
+						this.onInit();
+					}
+				}
+			},
+			// navigate back to the homepage
+			onHome: function(){
+				this.getOwnerComponent().getRouter().navTo("home");
+			},
+			onBeforeRebindTable: function(){
+				this.getOwnerComponent().getModel().refresh(true);
+			}
+  	});
+});
