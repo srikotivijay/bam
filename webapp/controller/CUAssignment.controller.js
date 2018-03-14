2c6c4ee0b9cc83fb2c6e23dd66de2b7c26d365c2
Prashanth Krishnan <pkrishnan5@dow.com>
2018-03-14 15:23:58 +0000

    modified the cu assignment for security

--- webapp/controller/CUAssignment.controller.js
+++ webapp/controller/CUAssignment.controller.js
@@ -8,13 +8,31 @@ sap.ui.define([
   	var loggedInUserID;
 	var firstTimePageLoad = true;
 	return Controller.extend("bam.controller.CUAssignment", {
-			onInit : function () {
-				// Get logged in user id
-			    loggedInUserID = DataContext.getUserID();
-				 // define a global variable for the oData model		    
-		    	var oView = this.getView();
-		    	oView.setModel(this.getOwnerComponent().getModel());
-	    		
+		onInit : function () {
+			// Get logged in user id
+		    loggedInUserID = DataContext.getUserID();
+			 // define a global variable for the oData model		    
+		   	var oView = this.getView();
+		   	oView.setModel(this.getOwnerComponent().getModel());
+	    	//
+	    	// checking the permission
+	    	var maintainRule = this._oi18nModel.getProperty("Module.maintainRules");
+			var permissions = DataContext.getUserPermissions();
+			var hasAccess = false;
+			for(var i = 0; i < permissions.length; i++)
+			{
+				if(permissions[i].ATTRIBUTE === maintainRule)
+				{
+						hasAccess = true;
+						break;
+				}
+			}
+			//
+			// if the user does not have access then redirect to accessdenied page
+			if(hasAccess === false){
+				this.getOwnerComponent().getRouter().navTo("accessDenied");
+			}
+			else{
 	    		//remove the selection column
 	    		var oSmartTable = this.getView().byId("smartTblCUAssignment");     //Get Hold of smart table
 				var oTable = oSmartTable.getTable();          //Analytical Table embedded into SmartTable
@@ -27,36 +45,38 @@ sap.ui.define([
 		    		//attach _onRouteMatched to be called everytime on navigation to Maintain Attributes page
 		    		var oRouter = this.getRouter();
 					oRouter.getRoute("cuAssignment").attachMatched(this._onRouteMatched, this);
-		    	}
-			},
-			getRouter : function () {
-				return sap.ui.core.UIComponent.getRouterFor(this);
-			},
-			// force init method to be called everytime we naviagte to Maintain Attribuets page 
-			_onRouteMatched : function (oEvent) {
-				// If the user does not exist in the BAM database, redirect them to the denied access page
-				if(DataContext.isBAMUser() === false)
+		    	}				
+			}
+
+		},
+		getRouter : function () {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		// force init method to be called everytime we naviagte to Maintain Attribuets page 
+		_onRouteMatched : function (oEvent) {
+			// If the user does not exist in the BAM database, redirect them to the denied access page
+			if(DataContext.isBAMUser() === false)
+			{
+				this.getOwnerComponent().getRouter().navTo("accessDenied");
+			}
+			else
+			{
+				if(firstTimePageLoad)
 				{
-					this.getOwnerComponent().getRouter().navTo("accessDenied");
+					firstTimePageLoad = false;
 				}
 				else
 				{
-					if(firstTimePageLoad)
-					{
-						firstTimePageLoad = false;
-					}
-					else
-					{
-						this.onInit();
-					}
+					this.onInit();
 				}
-			},
-			// navigate back to the homepage
-			onHome: function(){
-				this.getOwnerComponent().getRouter().navTo("home");
-			},
-			onBeforeRebindTable: function(){
-				this.getOwnerComponent().getModel().refresh(true);
 			}
+		},
+		// navigate back to the homepage
+		onHome: function(){
+			this.getOwnerComponent().getRouter().navTo("home");
+		},
+		onBeforeRebindTable: function(){
+			this.getOwnerComponent().getModel().refresh(true);
+		}
   	});
 });
