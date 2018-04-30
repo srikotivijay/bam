b0763b34d4c31061b7c0785e31f1fd471fb526e8
Prashanth Krishnan <pkrishnan5@dow.com>
2018-04-30 15:18:34 +0000

    partial submit

--- webapp/controller/EditUserMultiple.controller.js
+++ webapp/controller/EditUserMultiple.controller.js
@@ -55,7 +55,7 @@ sap.ui.define([
 			}
 			else
 			{
-	    		this.setPageToInitialState();
+	    		//this.setPageToInitialState();
 				//get current list of ids from model
 		    	var core = sap.ui.getCore();
 		    	var globalModel = core.getModel();
@@ -91,43 +91,169 @@ sap.ui.define([
 				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 				oRouter.navTo("userManagement", true);
 			}
+		},
+		//cancel click on edit attributes page
+		onCancel: function(){
+			var curr = this;
+			//get the list of updated attributes
+			var updatedUserList = curr.getUpdatedUsers();
+			// check if there are any changes to be updated
+			if (updatedUserList !== ""){
+				// check if user wants to update the attributes for GMID and country
+				MessageBox.confirm("Are you sure you want to cancel your changes and navigate back to the previous page?", {
+	            	icon: sap.m.MessageBox.Icon.WARNING,
+	            	actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+	            	onClose: function(oAction) {
+	            		if(oAction === "YES"){
+	            			curr.getOwnerComponent().getRouter().navTo("userManagement");
+	            		}
+	            	}
+	        	});
+			}
+			else{
+				curr.getOwnerComponent().getRouter().navTo("userManagement");
+			}
+		},
+		//get the list of updated attributes in string format
+		getUpdatedUsers: function(){
+			// get the crop protection and seeds value from i18n file
+	    	var oi18nModel = this.getView().getModel("i18n");
+			var updatedUserString = "";
+			if (this.validateUserValueChange("ddlAddRole")){
+				updatedUserString += oi18nModel.getProperty("ddRole");
+				updatedUserString += ", ";
+			}
+			if (this.validateUserValueChange("ddlRemoveRole")){
+				updatedUserString += oi18nModel.getProperty("ddRole");
+				updatedUserString += ", ";
+			}
+			return updatedUserString.substring(0, updatedUserString.length - 2);
+		},
+		//validate if the value of various attributes has been updated
+		validateUserValueChange: function (sourceControlName){
+			var type = sourceControlName.substring(0,3);
+			if((type === "ddl" && this.getView().byId(sourceControlName).getSelectedKeys().length > 0) || (type === "txt" && this.getView().byId(sourceControlName).getValue().trim() !== "")){
+				return true;
+			}
+			else{
+				return false;
+			}
+		},
+		//click of submit button
+		onSubmit: function(){
+			var curr = this;
+			//get the list of updated attributes
+			var updatedUserList = curr.getUpdatedUsers();
+			// check if there are any changes to be updated
+			if (updatedUserList !== ""){
+				// check if user wants to update the attributes for GMID and country
+				MessageBox.confirm(("Roles will be updated for " + curr._oViewModelData.USER_COUNT + " users. Continue?"), {
+	    			icon: sap.m.MessageBox.Icon.WARNING,
+	    			actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
+	          		onClose: function(oAction) {
+	          			var editUserIDList = curr._oViewModelData.EDIT_USER_ID_LIST;
+	        			curr.fnCallbackSubmitConfirm(oAction, editUserIDList);
+	            	}
+	       		});
+			}
+			else{
+				// check if user wants to update the attributes for GMID and country
+				MessageBox.alert("There are no pending changes", {
+	    			icon : MessageBox.Icon.ERROR,
+					title : "Error"
+	       		});
+			}
+		},
+		// update the attributes based on user response
+		fnCallbackSubmitConfirm: function(oAction, editUserIDList){
+			var curr = this;
+			var successUpdate = true;
+			var successCount = 0;
+			var updGMIDCountry = curr.createUpdateObject();
+			// //if user confirmed to update the attributes, prepare the object and update the attributes for the GMID and country
+			// //else do nothing
+			if (oAction === "YES") 
+			{
+			// 	// create a batch array and push each updated GMID to it
+			// 	var batchArray = [];
+			// 	for(var i = 0; i < editUserIDList.length; i++) 
+			//     {
+			//     	batchArray.push(this._oDataModel.createBatchOperation("GMID_SHIP_TO_COUNTRY(" + editUserIDList[i].ID + ")", "MERGE", updGMIDCountry));
+			//     	successCount++;
+			// 	}
+			// 	this._oDataModel.addBatchChangeOperations(batchArray);
+				
+			// 	// creating busy dialog lazily
+			// 	if (!this._busyDialog) 
+			// 	{
+			// 		this._busyDialog = sap.ui.xmlfragment("bam.view.BusyLoading", this);
+			// 		this.getView().addDependent(this._dialog);
+			// 	}
+				
+			// 	// setting to a local variable since we are closing it in an oData success function that has no access to global variables.
+			// 	var busyDialog = this._busyDialog;
+			// 	busyDialog.open();
+				
+			// 	// submit the batch update command
+			// 	this._oDataModel.submitBatch(
+			// 		function(oData,oResponse)
+			// 		{
+			// 			busyDialog.close();
+			// 			MessageBox.alert("Attributes for " + successCount + " Material/Country combinations updated successfully.",
+			// 				{
+			// 					icon : MessageBox.Icon.SUCCESS,
+			// 					title : "Success",
+			// 					onClose: function() {
+				        			curr.getOwnerComponent().getRouter().navTo("userManagement");
+			// 	        	}
+			// 			});
+			//     	},
+			//     	function(oError)
+			//     	{
+			//     		busyDialog.close();
+		 //   			MessageBox.alert("Error updating attributes for Material/Country combinations.",
+			// 			{
+			// 				icon : MessageBox.Icon.ERROR,
+			// 				title : "Error"
+			// 			});
+			//     	}
+			//     );
+
+				
+			}
+		},
+		createUpdateObject: function(userIdList){
+			// Create current timestamp
+			var oDate = new Date();
+			var UpdatedUserList = [];
+			for(var i = 0; i < userIdList.length; i++){
+				var addedRoleList = this.getView().byId("ddlAddRole").getSelectedKeys();
+				for(var j = 0; j < addedRoleList.length; j++){
+					var addedRole = {
+						USER_ID : userIdList[i],
+						ROLE_CODE_ID : addedRoleList[j],
+						IS_ADDED : "T",
+						MODIFIED_BY : loggedInUserID,
+						MODIFIED_ON : oDate
+					};
+					UpdatedUserList.push(addedRole);
+				}
+				//
+				var removedRoleList = this.getView().byId("ddlRemoveRole").getSelectedKeys();
+				for(var k = 0; k < removedRoleList.length; k++){
+					var removedRole = {
+						USER_ID : removedRoleList[i],
+						ROLE_CODE_ID : removedRoleList[k],
+						IS_ADDED : "F",
+						MODIFIED_BY : loggedInUserID,
+						MODIFIED_ON : oDate
+					};
+					UpdatedUserList.push(removedRole);
+				}
+			}    	
+			//return the object of updated attributes
+			return UpdatedUserList;
 		}
-
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf bam.view.EditUserMultiple
-		 */
-		//	onInit: function() {
-		//
-		//	},
-
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf bam.view.EditUserMultiple
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf bam.view.EditUserMultiple
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf bam.view.EditUserMultiple
-		 */
-		//	onExit: function() {
-		//
-		//	}
-
 	});
 
 });
