484010376c56efe39f33b0ff294c7b41e2a21f42
Prashanth Krishnan <pkrishnan5@dow.com>
2018-05-24 09:55:41 +0000

    attribute filter

--- webapp/controller/RuleReport.controller.js
+++ webapp/controller/RuleReport.controller.js
@@ -13,6 +13,8 @@ sap.ui.define([
 	var firstTimePageLoad = true;
 	var dateFilter = [];
 	var filter = [];
+	var beginInitialColumns = "GMID,GMID_DESC,SHIP_TO_COUNTRY";
+	var endInitialColumns = "CHANGED_BY,CHANGED_ON";
 	return Controller.extend("bam.controller.RuleReport", {
 			onInit : function () {
 				
@@ -21,18 +23,32 @@ sap.ui.define([
 				 // define a global variable for the oData model		    
 		    	var oView = this.getView();
 		    	oView.setModel(this.getOwnerComponent().getModel());
+		    	this._oi18nModel = this.getOwnerComponent().getModel("i18n");
 	    		
 	    		//remove the selection column
-	    		var oSmartTable = this.getView().byId("smartTblRuleReport");     //Get Hold of smart table
-				var oTable = oSmartTable.getTable();          //Analytical Table embedded into SmartTable
+	    		this._oSmartTable  = this.getView().byId("smartTblRuleReport");     //Get Hold of smart table
+				var oTable = this._oSmartTable.getTable();          //Analytical Table embedded into SmartTable
+				var oSmartFilterbar = this.getView().byId("smartFilterBar");
+				oSmartFilterbar.clear(); // clear the existing filters
 				oTable.setSelectionMode("None");
 				oTable.setEnableColumnFreeze(true);
+				
+				this._oModel = new sap.ui.model.json.JSONModel();
+				this.getView().setModel(this._oModel,"RuleAuditVM");
+				this._oDataModel = new sap.ui.model.odata.ODataModel("/ODataService/BAMDataService.xsodata/", true);
+			    this._oModel.setProperty("/ChangeAttributes",this.getChangeAttributeDropDown());
+			    //
+			    //var initialColumns = this.getTableColumns("V_WEB_CU_PEOPLE_RULE_LOG").join();
+			    //this._oSmartTable.setInitiallyVisibleFields(initialColumns);
 				
 		    	if(firstTimePageLoad)
 		    	{
 		    		//attach _onRouteMatched to be called everytime on navigation to Maintain Attributes page
 		    		var oRouter = this.getRouter();
 					oRouter.getRoute("ruleReport").attachMatched(this._onRouteMatched, this);
+		    	}
+		    	else{
+		    		this.setSmartTablePersonalization(false);
 		    	}
 			},
 			getRouter : function () {
@@ -86,33 +102,259 @@ sap.ui.define([
 	                });
 	                aFilters.push(dateFilters);
 				}
+				
+				if(filter.length > 0){
+	            	var gmidFilterList = new Filter ({
+	                    filters : filter,
+	                        bAnd : false
+	                    });
+		            if(aFilters.length > 0 && aFilters[0].aFilters !== undefined){
+		            	aFilters[0].bAnd = true;
+		            	aFilters[0].aFilters.push(gmidFilterList);
+		            }
+		            else{
+		            	aFilters.push(gmidFilterList);
+		            }
+				}
 			},
 			smartFilterSearch: function(oEvent){
 				var filterArray = [];
 				filter = [];
 				dateFilter = [];
+				var changeAttributeList = this.getView().byId("cmbAttr").getSelectedKeys();
 				
-				try{
-					var fromDate = this.getView().byId("DPF")._getSelectedDate();
+				var fromDate = this._oModel.getProperty("/DateTimeFrom");
+				if(fromDate !== undefined){
 					var fromDateFilter = new Filter("OPERATION_ON",sap.ui.model.FilterOperator.GE, fromDate);
 					dateFilter.push(fromDateFilter);
 				}
-				catch (err){
-					
-				}
 				
-				try{
-					var toDate = this.getView().byId("DPT")._getSelectedDate();
+				var toDate = this._oModel.getProperty("/DateTimeTo");
+				if(toDate !== undefined){
 					toDate.setHours(toDate.getHours() + 23);
 					toDate.setMinutes(toDate.getMinutes() + 59);
 					toDate.setSeconds(toDate.getSeconds() + 59);
 					var toDateFilter = new Filter("OPERATION_ON",sap.ui.model.FilterOperator.LE, toDate);
 					dateFilter.push(toDateFilter);
 				}
-				catch (err){
-					
+				if(changeAttributeList.length > 0){
+					var attributes = this.getAttributeMapping(changeAttributeList);
+					var attributeColumns = [];
+					for(var i = 0; i < attributes.length; i++){
+						attributeColumns.push(attributes[i].MAPPED_ATTRIBUTE_NAME);
+						if(attributes[i].MAPPED_ATTRIBUTE_NAME.includes("PRIOR")){
+							var changeFilter = new Filter(attributes[i].MAPPED_ATTRIBUTE_NAME,sap.ui.model.FilterOperator.NE, "NO CHANGE");
+							filter.push(changeFilter);
+						}
+					}
+					var selectedAttributeColumns = beginInitialColumns + "," + attributeColumns.join() +  "," + endInitialColumns;
+					var additionalColumns = this.findEligibleColumns(selectedAttributeColumns);
+					if(additionalColumns.length > 0){
+						selectedAttributeColumns = beginInitialColumns + "," + attributeColumns.join() + "," + additionalColumns.join() + "," + endInitialColumns;
+					}
+					this.showHideColumns(selectedAttributeColumns);
 				}
+				else{
+					this.setSmartTablePersonalization(true);
+				}
+			},
+			getChangeAttributeDropDown : function () {
+				var result;
+				// Create a filter & sorter array
+				var filterArray = [];
+				var moduleFilter = new Filter("MODULE_CODE_KEY",sap.ui.model.FilterOperator.EQ,"RULE_HISTORY");
+				filterArray.push(moduleFilter);
+				var sortArray = [];
+				var sorter = new sap.ui.model.Sorter("ATTRIBUTE_LABEL",false);
+				sortArray.push(sorter);
+				// Get the Country dropdown list from the CODE_MASTER table
+				this._oDataModel.read("/V_CHANGE_ATTRIBUTE_MODULE_MAPPING",{
+						filters: filterArray,
+						sorters: sortArray,
+						async: false,
+		                success: function(oData, oResponse){
+			                // Bind the Geography data
+			                result =  oData.results;
+		                },
+		    		    error: function(){
+	    		    		MessageBox.alert("Unable to retrieve dropdown values for Geo Level Please contact System Admin.",
+							{
+								icon : MessageBox.Icon.ERROR,
+								title : "Error"
+							});
+		            		result = [];
+		    			}
+		    	});
+		    	if(result[0] !== undefined){
+		    		this._moduleId = result[0].MODULE_ID;
+		    	}
+		    	return result;
+			},
+			getAttributeMapping : function (changeAttributeList) {
+				var fullAttributeList = [];
+				var sortArray = [];
+				var sorter = new sap.ui.model.Sorter("ORDER",false);
+				sortArray.push(sorter);
+				for(var i = 0; i < changeAttributeList.length; i++){
+					var filterArray = [];
+					var attrFilter = new Filter("CHANGE_ATTRIBUTE_ID",sap.ui.model.FilterOperator.EQ, changeAttributeList[i]);
+					filterArray.push(attrFilter);
+					var modFilter = new Filter("MODULE_ID",sap.ui.model.FilterOperator.EQ, this._moduleId);
+					filterArray.push(modFilter);
 				
+					var result = [];
+					this._oDataModel.read("/CHANGE_ATTRIBUTE_MAPPING",{
+						filters: filterArray,
+						sorters: sortArray,
+						async: false,
+		                success: function(oData, oResponse){
+			                // Bind the Geography data
+			                result =  oData.results;
+		                },
+		    		    error: function(){
+	    		    		MessageBox.alert("Unable to retrieve dropdown values for Geo Level Please contact System Admin.",
+							{
+								icon : MessageBox.Icon.ERROR,
+								title : "Error"
+							});
+		            		result = [];
+		    			}
+		    		});
+		    		for(var c = 0; c < result.length; c++){
+		    			fullAttributeList.push(result[c]);
+		    		}
+				}
+				// Get the Country dropdown list from the CODE_MASTER table
+		    	return fullAttributeList;
+			},
+			findEligibleColumns: function(filterColumns){
+				filterColumns = filterColumns.split(",");
+				var eligibleColumns = this._oSmartTable._oPersController.oModels.$sapuicomppersonalizationBaseController.oData.controlData.columns.columnsItems; // eslint-disable-line
+				var result = []; 
+				for(var i = 0; i < eligibleColumns.length; i++){
+					var currentColumn = eligibleColumns[i];
+					if(currentColumn.visible === true){
+						var addFlag = true;
+						for(var a = 0; a < filterColumns.length; a++){
+							if(filterColumns[a] === currentColumn.columnKey){
+								addFlag = false;
+								break;
+							}
+						}
+						if(addFlag){
+							result.push(currentColumn.columnKey);
+						}
+					}
+				}
+				return result;
+			},
+			showHideColumns: function(visibleFields){
+				// var oSmartTable = this.getView().byId("smartTblRuleReport");     //Get Hold of smart table
+				// var tableId = oSmartTable.getId() + "-";
+				// var columns = oSmartTable.getTable().getColumns();
+				
+				// var table = this._oSmartTable.getTable();
+				// var visibleFieldCollection = visibleFields.split(",");
+
+				// this.setSmartTablePersonalization(false);
+				// //remove and insert for proper order then set visible
+				// for(var i = 0; i < visibleFieldCollection.length; i++){
+				// 	var controlData = this._oSmartTable._oPersController.oModels.$sapuicomppersonalizationBaseController.oData.controlData.columns.columnsItems.find(function(ele){ return ele.columnKey === visibleFieldCollection[i];}); 	// eslint-disable-line
+				// 	if(controlData !== undefined){
+				// 		controlData.visible = true;
+				// 		controlData.index = i;
+				// 	}
+				// 	var alreadyKnownPersistentData = this._oSmartTable._oPersController.oModels.$sapuicomppersonalizationBaseController.oData.alreadyKnownPersistentData.columns.columnsItems.find(function(ele){ return ele.columnKey === visibleFieldCollection[i];}); 	// eslint-disable-line
+				// 	if(alreadyKnownPersistentData !== undefined){
+				// 		alreadyKnownPersistentData.visible = true;
+				// 		alreadyKnownPersistentData.index = i;
+				// 	}
+				// 	var alreadyKnownRuntimeData = this._oSmartTable._oPersController.oModels.$sapuicomppersonalizationBaseController.oData.alreadyKnownRuntimeData.columns.columnsItems.find(function(ele){ return ele.columnKey === visibleFieldCollection[i];}); 	// eslint-disable-line
+				// 	if(alreadyKnownRuntimeData !== undefined){
+				// 		alreadyKnownRuntimeData.visible = true;
+				// 		alreadyKnownRuntimeData.index = i;
+				// 	}
+				// 	var controlDataBase = this._oSmartTable._oPersController.oModels.$sapuicomppersonalizationBaseController.oData.controlDataBase.columns.columnsItems.find(function(ele){ return ele.columnKey === visibleFieldCollection[i];}); 	// eslint-disable-line
+				// 	if(controlDataBase !== undefined){
+				// 		controlDataBase.visible = true;
+				// 		controlDataBase.index = i;
+				// 	}
+				// 	var column = columns.find(function(ele){ return ele.getId().replace(tableId,"") === visibleFieldCollection[i];});
+				// 	if(column !== undefined){
+				// 		table.removeColumn(column);
+				// 		//var c_column = column.setVisible(true);
+				// 		table.insertColumn(column, i);
+				// 		column.setVisible(true);
+				// 	}
+				// }
+			},
+			setSmartTablePersonalization: function(visible){
+				// var columns = this._oSmartTable.getTable().getColumns();
+				// for(var i = 0; i < columns.length; i++){ columns[i].setVisible(visible);}
+				// var controlData = this._oSmartTable._oPersController.oModels.$sapuicomppersonalizationBaseController.oData.controlData.columns.columnsItems; 	// eslint-disable-line
+				// for(var i = 0; i < controlData.length; i++){
+				// 	var current = controlData[i];
+				// 	current.visible = visible;
+				// 	current.index = visible ? i : -1;
+				// }
+				// var alreadyKnownPersistentData = this._oSmartTable._oPersController.oModels.$sapuicomppersonalizationBaseController.oData.alreadyKnownPersistentData.columns.columnsItems; 	// eslint-disable-line
+				// for(var i = 0; i < alreadyKnownPersistentData.length; i++){
+				// 	var current = alreadyKnownPersistentData[i];
+				// 	current.visible = visible;
+				// 	current.index = visible ? i : -1;
+				// }
+				// var alreadyKnownRuntimeData = this._oSmartTable._oPersController.oModels.$sapuicomppersonalizationBaseController.oData.alreadyKnownRuntimeData.columns.columnsItems; 	// eslint-disable-line
+				// for(var i = 0; i < alreadyKnownRuntimeData.length; i++){
+				// 	var current = alreadyKnownRuntimeData[i];
+				// 	current.visible = visible;
+				// 	current.index = visible ? i : -1;
+				// }
+				// var controlDataBase = this._oSmartTable._oPersController.oModels.$sapuicomppersonalizationBaseController.oData.controlDataBase.columns.columnsItems; 	// eslint-disable-line
+				// for(var i = 0; i < controlDataBase.length; i++){
+				// 	var current = controlDataBase[i];
+				// 	current.visible = visible;
+				// 	current.index = visible ? i : -1;
+				// }
+			},
+			tableInitialised: function(){
+				// var allColumns = this._oSmartTable.getTable().getColumns();
+				// for(var i = 0; i < allColumns.length; i++){ allColumns[i].setVisible(false);}
+			
+				// this.setSmartTablePersonalization(false);
+			},
+			getTableColumns : function (tableName) {
+				var filterArray = [];
+				var environmentFilter = new Filter("SCHEMA_NAME",sap.ui.model.FilterOperator.EQ,this._oi18nModel.getProperty("environment"));
+				filterArray.push(environmentFilter);
+				var tableFilter = new Filter("VIEW_NAME",sap.ui.model.FilterOperator.EQ,tableName);
+				filterArray.push(tableFilter);
+				var sortArray = [];
+				var sorter = new sap.ui.model.Sorter("POSITION",false);
+				sortArray.push(sorter);
+				var result = [];
+				// Get the Country dropdown list from the CODE_MASTER table
+				this._oDataModel.read("/VIEW_COLUMNS",{
+						filters: filterArray,
+						sorters: sortArray,
+						async: false,
+		                success: function(oData, oResponse){
+			                // Bind the Geography data
+			                for (var i = 0; i < oData.results.length; i++){
+			                	result.push(oData.results[i].COLUMN_NAME);
+			                }
+			                // result =  oData.results;
+		                },
+		    		    error: function(){
+	    		    		MessageBox.alert("Unable to retrieve dropdown values for Geo Level Please contact System Admin.",
+							{
+								icon : MessageBox.Icon.ERROR,
+								title : "Error"
+							});
+		            		result = [];
+		    			}
+		    	});
+		    	result.splice(result.findIndex(function(ele){ return ele === "ID";}),1);
+		    	return result;
 			}
   	});
 });
